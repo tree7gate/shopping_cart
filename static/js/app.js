bdcf68c4e5dc6dd36f776fe980a2b721d2a27734
Chet Lynch <chetlynch@Chets-MBP.hsd1.ma.comcast.net>
2018-10-01 14:04:55 -0400

    properly calculating total and using jQuery to insert where needed

--- static/js/app.js
+++ static/js/app.js
@@ -65,6 +65,7 @@ function addToCart(id) {
   // Usage!
   sleep(50).then(() => {
       showCart();
+      displayTotal();
   });
 
 }
@@ -97,7 +98,7 @@ function showCart() {
 
 }
 
-// TODO: create remove functionality
+
 function removeFromCart(id) {
   // loop through products in cart and remove one instance of id
   for (let i = 0; i < cart.length; i++) {
@@ -111,8 +112,32 @@ function removeFromCart(id) {
   // run showCart to generate current cart array
   sleep(50).then(() => {
       showCart();
+      displayTotal();
   });
 }
+
+
+function getTotal() {
+  let total = 0;
+  // loop through products in cart and add prices
+  for(let i = 0; i < cart.length; i++){
+    total += cart[i].price;
+  }
+  return total.toFixed(2);
+}
+
+// TODO: display correct total in proper locations
+function displayTotal() {
+  // get total and store in variable
+  let total = getTotal();
+
+  // change total in navbar
+  $("#nav-total").text(`Total: $${total}`);
+
+  // change total in cart
+  $("#cart-total").text(`Total: $${total}`);
+}
+
 
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
